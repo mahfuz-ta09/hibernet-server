8a439fd61074ce4e004efa7f260ef542696294bb
Md Mahfuz Anam Tasnim <mahfuz.ta09@gmail.com>
2024-11-17 18:02:31 +0600

    upload field created

--- server.ts
+++ server.ts
@@ -45,6 +45,17 @@ app.all('*',(req:Request,res:Response) =>{
     }
 })
 
+// app.all('*',(req:Request,res:Response) =>{
+//     res.status(404)
+//     if(req.accepts('html')){
+//       res.sendFile(path.join(__dirname, 'views' , '404.html'))
+//     }else if(req.accepts('json')){
+//       res.json({ message: '404 Not Found'})
+//     }else{
+//       res.type('txt').send('404 Not Found')
+//     }
+// })
+
 
 app.use(errorHandler)
 
